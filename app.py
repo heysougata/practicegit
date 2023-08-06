c3441b028e2476e6f44932f829fca20f32bca637
Sougata Patra <heysougata07@gmail.com>
2023-08-06 21:36:47 +0530

    this is new update app.py

--- app.py
+++ app.py
@@ -1,9 +1,9 @@
-def sum(a, b):
-    return a + b
+def division(a, b):
+    return a / b
 
 if __name__ == "__main__":
+    print(division(10, 2))
+    
+    print(division(10, 0))
 
-    print(sum(1, 2))
-
-def mul(a, b):
-    return a * b
+# it is important division code
